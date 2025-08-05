fb1156863205a5933593ae10b3d1571fbbe9e32b
MysteryCodes <39903046+DKTJONATHAN@users.noreply.github.com>
2025-08-05 09:38:40 +0300

    Update postsStore.js

--- src/store/postsStore.js
+++ src/store/postsStore.js
@@ -1,1 +1,12 @@
-.
+import { create } from 'zustand'
+
+const usePostsStore = create((set) => ({
+  posts: [],
+  post: null,
+  loading: false,
+  setPosts: (posts) => set({ posts }),
+  setPost: (post) => set({ post }),
+  setLoading: (loading) => set({ loading }),
+}))
+
+export default usePostsStore
