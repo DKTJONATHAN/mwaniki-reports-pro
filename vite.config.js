51fa1ae9df24721041dcd16d8a326013b65a7eea
MysteryCodes <39903046+DKTJONATHAN@users.noreply.github.com>
2025-08-05 10:56:21 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+    historyApiFallback: true,
   },
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html')
+      }
+    }
   },
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@public': path.resolve(__dirname, './public')
+    }
+  },
+  publicDir: 'public'
+});
