6a0d32647a4b122a675ce49f3565b0d61dbeeb64
MysteryCodes <39903046+DKTJONATHAN@users.noreply.github.com>
2025-08-05 10:36:04 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { HelmetProvider } from 'react-helmet-async'
-import App from './App'
-import './styles/index.css'
+import App from './App.jsx'
+import './index.css'
 
+// Create root and render app
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </HelmetProvider>
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
