9ed642fbdb3c102c965daf3b21e2ea6fe9570baa
MysteryCodes <39903046+DKTJONATHAN@users.noreply.github.com>
2025-08-05 09:35:30 +0300

    Update seoUtils.js

--- src/utils/seoUtils.js
+++ src/utils/seoUtils.js
@@ -1,1 +1,16 @@
-.
+export function generateMetaTags({ title, description, keywords, image }) {
+  return {
+    title: `${title} | Mwaniki's Report`,
+    meta: [
+      { name: 'description', content: description },
+      { name: 'keywords', content: keywords },
+      { name: 'author', content: 'Jonathan Mwaniki' },
+      { property: 'og:title', content: title },
+      { property: 'og:description', content: description },
+      { property: 'og:image', content: image || '/images/Jonathan-Mwaniki-logo.png' },
+      { property: 'og:url', content: 'https://jonathanmwaniki.co.ke' },
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { name: 'twitter:creator', content: '@maestropuns' },
+    ],
+  }
+}
